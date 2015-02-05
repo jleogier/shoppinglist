259cbe47ff904ecc9f80142fc9a544746a04364c
jleogier <jeremy.leogier@gmail.com>
2015-02-04 23:09:56 -0800

    Shopping List commit

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
-	console.log("hi");
+$(document).ready(function() {	
 
-	$("ul li").on('click', function(clicked) {
-		console.log("hello");
-	})
-})
+
+	$("#submitButton").click(function(e){
+		e.preventDefault();
+
+		item = $("#itemEntered").val();
+		$("#itemEntered").val('');
+
+		$("#list").append("<li>" + item + "</li>");
+		console.log(item);		
+	});
+	
+
+
+
+
+
+//	$('ul').on('click', 'li', function(clicked) {
+//		$("li").remove();
+//	});
+
+
+	$('#list').on('dblclick', 'li', function(e) {	
+		$(e.target).remove();
+	});
+
+	$('#list').on('click', 'li', function(e){
+		$(e.target).addClass('completed');
+	});
+
+
+
+
+
+});
